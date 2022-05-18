d2e9797ae5ef5a0d4e2b86143bb436c723d564bf
guonan <838093891@qq.com>
2022-05-19 01:37:18 +0800

    style

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,21 @@ const dataList: ColumnProps = [
   {
     id: 1,
     title: 'Vue.js',
+    headimg: 'https://cn.vuejs.org/images/logo.svg',
     avatar: 'https://img.shields.io/badge/vue-js-blue.svg',
     description: 'this is vue.js'
   },
   {
     id: 2,
     title: 'React.js',
+    headimg: 'https://img1.baidu.com/it/u=3606169170,2721037717&fm=253&fmt=auto&app=138&f=PNG?w=500&h=500',
     avatar: 'https://img.shields.io/badge/react-js-blue.svg',
     description: 'this is react.js'
   },
   {
     id: 3,
     title: 'Angular.js',
+    headimg: 'https://img0.baidu.com/it/u=2276354322,369264578&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=557',
     avatar: 'https://img.shields.io/badge/angular-js-blue.svg',
     description: 'this is angular.js'
   },
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,35 +1,49 @@
 <template>
-  <div class="row">
-    <div v-for="column in list" :key="column.id" class="col-4">
-      <div class="card">
-          <img :src="column.avatar" :alt="column.title">
-          <div class="card-body" >
-            <p class="card-title">{{ column.title }}</p>
-            <p class="card-text">{{ column.description }}</p>
-            <p class="btn btn-primary">进入专栏</p>
-          </div>
+  <div class="row mt-4">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card-body text-center border">
+          <img :src="column.headimg" class="border w-25 my-3 border-light">
+          <p><img :src="column.avatar" :alt="column.title"></p>
+          <p class="card-title">{{ column.title }}</p>
+          <p class="card-text">{{ column.description }}</p>
+          <p class="btn btn-outline-primary">进入专栏</p>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, computed } from 'vue'
 export interface ColumnProps {
   id: number;
   title: string;
+  headimg?: string;
   avatar: string;
   description: string;
 }
-export default {
+export default defineComponent({
   name: 'ColumnList',
   props: {
     list: {
       type: Array as PropType<ColumnProps[]>,
       default: () => []
     }
+  },
+  setup(props) {
+    const columnList = computed(() => {
+      return props.list.map(column => {
+        if (!column.headimg) {
+          column.headimg = require('@/assets/avatar.jpg')
+        }
+        return column
+      })
+    })
+    console.log(columnList)
+    return {
+      columnList
+    }
   }
-}
+})
 </script>
 
 <style lang="scss" scoped></style>
